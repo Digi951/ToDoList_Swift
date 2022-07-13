16d19a3dafe3eade99c272138eddc3904c5498d4
digi <Ch.Dieckmann@outlook.de>
2022-07-13 21:02:25 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Christopher Dieckmann on 13.07.22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
